20f3056c6b02e485957571774465eaccd5d4fd29
Subrat Garnaik <122955567+JS-Swan@users.noreply.github.com>
2024-10-01 12:05:50 +0530

    Add files via upload

---
+++ 1.Newbie/4.Review Section/script.js
@@ -0,0 +1,71 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: 'https://www.course-api.com/images/people/person-1.jpeg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    img: 'https://www.course-api.com/images/people/person-2.jpeg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    img: 'https://www.course-api.com/images/people/person-4.jpeg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    img: 'https://www.course-api.com/images/people/person-3.jpeg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+const img=document.getElementById("person-img")
+const myname=document.getElementById("myname")
+
+const job=document.getElementById("job");
+const info=document.getElementById("info");
+
+const btnLeft=document.querySelector(".left")
+const btnRight=document.querySelector(".right")
+const randomBtn=document.querySelector(".random-btn")
+
+let index=1;
+
+window.addEventListener("DOMContentLoaded",()=>{
+  showPerson(index);
+})
+
+function showPerson(index){
+  img.src=reviews[index].img;
+   myname.innerText=reviews[index].name;
+   job.textContent=reviews[index].job;
+   info.textContent=reviews[index].text;
+}
+btnLeft.addEventListener("click",()=>{
+  index--;
+  if(index < 0){
+    index=reviews.length -1;
+  }
+  showPerson(index)
+})
+btnRight.addEventListener("click",()=>{
+  index++;
+  if(index > reviews.length-1){
+    index= 0;
+  }
+  showPerson(index)
+})
+randomBtn.addEventListener("click",()=>{
+  index=Math.floor(Math.random() * reviews.length);
+ showPerson(index);
+})
